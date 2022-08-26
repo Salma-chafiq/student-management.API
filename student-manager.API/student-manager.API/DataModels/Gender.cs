3b5c0da44a44fd09be338623e8d7539ea3b39dd7
salmachafiq <salmachafiq2020@gmail.com>
2022-08-26 14:20:28 +0100

    Initial Commit

---
+++ student-manager.API/student-manager.API/DataModels/Gender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace student_manager.API.DataModels
+{
+    public class Gender
+    {
+    }
+}
